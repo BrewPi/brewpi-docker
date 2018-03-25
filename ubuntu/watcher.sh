d8ab9141d1a02cc06474d6d5781099edae4b4489
Elco Jacobs <elco@brewpi.com>
2018-03-25 19:25:24 +0200

    fix permissions not set, which prevents deleting dont-run file.  Check script every 10 seconds.

--- ubuntu/watcher.sh
+++ ubuntu/watcher.sh
@@ -5,5 +5,5 @@ do
   if ! python /home/brewpi/brewpi.py --checkstartuponly --dontrunfile
     then python -u /home/brewpi/brewpi.py 1>/home/brewpi/logs/stdout.txt 2>>/home/brewpi/logs/stderr.txt
   fi
-  sleep 30
+  sleep 10
 done
